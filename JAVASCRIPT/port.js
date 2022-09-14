51e3c2533a52f5fada070545384a9b7e255ab0ed
Joaovictormart <martinsvjoaos@gmail.com>
2022-09-14 16:59:08 -0300

    first commit

---
+++ JAVASCRIPT/port.js
@@ -0,0 +1,16 @@
+
+let lista = document.querySelector(".lista");
+
+let menu = document.querySelector(".menu")
+
+
+
+
+menu.addEventListener("click",function hand() {
+
+  lista.classList.toggle("hj")
+lista.classList.toggle("Hdd")
+})
+
+
+
